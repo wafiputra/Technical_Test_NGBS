625e31cc8174bee57ad7c770da8b8ef90672eace
Whoopy <wparyansyah@gmail.com>
2023-08-14 13:58:57 +0700

    Carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Carousel, CarouselItem, CarouselOptions, CarouselInterface } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Technical_Test_NGBS';
+
+  ngOnInit(): void {
+    this.initCarousel();
+  }
+
+  initCarousel(): void {
+    const items: CarouselItem[] = [
+      {
+        position: 0,
+        el: document.getElementById('carousel-item-1')!
+      },
+      {
+        position: 1,
+        el: document.getElementById('carousel-item-2')!
+      },
+      {
+        position: 2,
+        el: document.getElementById('carousel-item-3')!
+      },
+      {
+        position: 3,
+        el: document.getElementById('carousel-item-4')!
+      },
+    ];
+
+    const options: CarouselOptions = {
+      defaultPosition: 1,
+    interval: 3000,
+
+    indicators: {
+        activeClasses: 'bg-white dark:bg-gray-800',
+        inactiveClasses: 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+        items: [
+            {
+                position: 0,
+                el: document.getElementById('carousel-indicator-1')!
+            },
+            {
+                position: 1,
+                el: document.getElementById('carousel-indicator-2')!
+            },
+            {
+                position: 2,
+                el: document.getElementById('carousel-indicator-3')!
+            },
+            {
+                position: 3,
+                el: document.getElementById('carousel-indicator-4')!
+            },
+        ]
+    },
+
+    // callback functions
+    onNext: () => {
+        console.log('next slider item is shown');
+    },
+    onPrev: ( ) => {
+        console.log('previous slider item is shown');
+    },
+    onChange: ( ) => {
+        console.log('new slider item has been shown');
+    }
+    };
+
+    const carousel: CarouselInterface = new Carousel(items, options);
+
+    carousel.cycle();
+
+    const $prevButton = document.getElementById('data-carousel-prev');
+    const $nextButton = document.getElementById('data-carousel-next');
+
+    $prevButton?.addEventListener('click', () => {
+      carousel.prev();
+    });
+
+    $nextButton?.addEventListener('click', () => {
+      carousel.next();
+    });
+  }
 }
